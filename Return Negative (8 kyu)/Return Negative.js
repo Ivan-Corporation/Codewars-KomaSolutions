0b25e5091ea2ee7b21f92d5d98e86de6eb104281
IvanKoma <58357980+Ivan-Corporation@users.noreply.github.com>
2021-03-28 19:08:38 +0700

    Add files via upload

---
+++ Return Negative (8 kyu)/Return Negative.js
@@ -0,0 +1,8 @@
+// JS
+function makeNegative(num) {
+  if (num < 0) {
+    return num;
+  } else {
+    return -num;
+  }
+}
---
+++ Return Negative (8 kyu)/Return Negative.ts
@@ -0,0 +1,4 @@
+// TS
+export const makeNegative = (num: number): number => {
+  return -Math.abs(num);
+};
